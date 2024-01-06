49fa506ee9be17eddc98796b855224bd36a73480
Recxo <rishava065@gmail.com>
2024-01-06 18:01:03 +0530

    added other stuff

--- data_ingest.py
+++ data_ingest.py
@@ -5,7 +5,7 @@ class IngestData:
     def __init__(self) -> None:
         self.data_path = None
 
-    def get_data(self,data_path) -> pd.DataFrame:
+    def get_data(self,data_path:str) -> pd.DataFrame:
         self.data_path = data_path
         df = pd.read_csv(self.data_path)
         return df 
